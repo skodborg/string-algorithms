b98978aa5252639e88c357f05a1d5b7a143f8175
skodborg <skod92@gmail.com>
2016-05-17 19:06:34 +0200

    added border_array function

--- pattern-matching.py
+++ pattern-matching.py
@@ -13,6 +13,24 @@ def simple_longest_border(aString):
     return borderlength
 
 
+def border_array(aString):
+    ''' returns a list of length n of integers i, where each int i describes
+        the longest border found in the string aString[0:i] '''
+    ba = [0]
+    n = len(aString)
+    x = aString
+
+    for i in range(1, n):
+        b = ba[i - 1]  # -1 to convert i from one- to zero-indexed list index
+        while b > 0 and x[i + 1 - 1] != x[b + 1 - 1]:  # -1's again to convert
+            b = ba[b - 1]  # -1 again to convert
+        if x[i + 1 - 1] == x[b + 1 - 1]:  # -1's again to convert
+            ba.append(b + 1)
+        else:
+            ba.append(0)
+    return ba
+
+
 def search_naive(aString, aPattern):
     ''' finds occurrences of aPattern in aString in time O(nm) and returns
         a list of one-indexed occurrences of aPattern in aString '''
@@ -39,6 +57,7 @@ def search_kmp(aString, aPattern):
 def main():
     # print(simple_longest_border('aabcdeaab'))
     # print(search_naive('mississippi', 'ss'))
+    print(border_array('aabcdaaecaabc'))
 
 if __name__ == '__main__':
     main()
