58118d336b13b3d2bda1ab4cc7c664771a3e1f59
skodborg <skod92@gmail.com>
2016-05-23 15:07:31 +0200

    cleaned up + added verification of correctness of implementations

--- pattern-matching.py
+++ pattern-matching.py
@@ -1,5 +1,7 @@
 import time
 import suffix_tree as st
+import profiling as pf
+import re
 
 
 def simple_longest_border(aString):
@@ -112,30 +114,6 @@ def search_kmp(aString, aPattern):
 
 def verify_naive_Onm():
 
-    # results:
-
-    # marc@skodborg:~/Documents/AU/Git/string-algorithms$ python3 pattern-matching.py
-    # p10 result median of 10 runs: 2.932652
-    # [2.851702928543091, 2.8580031394958496, 2.8740298748016357, 2.908234119415283, 2.932651996612549, 2.9392731189727783, 2.954724073410034, 2.9603519439697266, 2.960646867752075, 2.978085994720459]
-
-    # p100 result median of 10 runs: 2.991202
-    # [2.940295934677124, 2.948166847229004, 2.9726250171661377, 2.9734160900115967, 2.9912021160125732, 2.9928479194641113, 2.99674391746521, 3.008416175842285, 3.008962869644165, 3.030972957611084]
-
-    # p1000 result median of 10 runs: 3.830846
-    # [3.8139729499816895, 3.8151941299438477, 3.826303005218506, 3.826917886734009, 3.830845832824707, 3.846817970275879, 3.8474650382995605, 3.8650670051574707, 3.872408151626587, 3.8835649490356445]
-
-    # p10000 result median of 10 runs: 5.706376
-    # [5.673080205917358, 5.696889162063599, 5.697651147842407, 5.7003090381622314, 5.70637583732605, 5.716675043106079, 5.717229127883911, 5.718000173568726, 5.722609043121338, 5.75190806388855]
-
-    # p100000 result median of 3 runs: 56.763344
-    # [56.70022010803223, 56.763344049453735, 56.80017900466919]
-
-    # p1000000 result median of 3 runs: 721.457279
-    # [720.9139211177826, 721.4572789669037, 722.2307889461517]
-
-
-    # tag faktiske kørselstider for forskellige m og sammenlign i graf
-    # med O(nm) ved at plotte kørselstid/(n*m) for forskellige m
     input = open('project3/chr1_10000000.txt', 'r', encoding='latin1').read()
     p10 = open('project3/pattern10.txt', 'r', encoding='latin1').read()
     p20 = open('project3/pattern20.txt', 'r', encoding='latin1').read()
@@ -152,187 +130,36 @@ def verify_naive_Onm():
     p500000 = open('project3/pattern500000.txt', 'r', encoding='latin1').read()
     p1000000 = open('project3/pattern1000000.txt', 'r', encoding='latin1').read()
 
-    p10_times = []
-    for _ in range(10):
-        start_time = time.time()
-        # search_naive(input, p10)
-        search_ba(input, p10)
-        # search_kmp(input, p10)
-        end_time = time.time()
-        p10_times.append(end_time - start_time)
-    p10_times.sort()
-    print('p10 result median of 10 runs: %f' % p10_times[4])
-    print(p10_times)
-    print()
+    patterns = [p10, p20, p30, p40, p50, p100, 
+                p500, p1000, p5000, p10000, p50000, 
+                p100000, p500000, p1000000]
 
-    p20_times = []
-    for _ in range(10):
-        start_time = time.time()
-        # search_naive(input, p20)
-        search_ba(input, p20)
-        # search_kmp(input, p20)
-        end_time = time.time()
-        p20_times.append(end_time - start_time)
-    p20_times.sort()
-    print('p20 result median of 20 runs: %f' % p20_times[4])
-    print(p20_times)
-    print()
+    for p in patterns:
+        naive_times = []
+        ba_times = []
+        kmp_times = []
+        for _ in range(3):
+            start_time = time.time()
+            search_naive(input, p)
+            end_time = time.time()
+            naive_times.append(end_time - start_time)
 
-    p30_times = []
-    for _ in range(10):
-        start_time = time.time()
-        # search_naive(input, p30)
-        search_ba(input, p30)
-        # search_kmp(input, p30)
-        end_time = time.time()
-        p30_times.append(end_time - start_time)
-    p30_times.sort()
-    print('p30 result median of 30 runs: %f' % p30_times[4])
-    print(p30_times)
-    print()
+            start_time = time.time()
+            search_ba(input, p)
+            end_time = time.time()
+            ba_times.append(end_time - start_time)
 
-    p40_times = []
-    for _ in range(10):
-        start_time = time.time()
-        # search_naive(input, p40)
-        search_ba(input, p40)
-        # search_kmp(input, p40)
-        end_time = time.time()
-        p40_times.append(end_time - start_time)
-    p40_times.sort()
-    print('p40 result median of 40 runs: %f' % p40_times[4])
-    print(p40_times)
-    print()
-
-    p50_times = []
-    for _ in range(10):
-        start_time = time.time()
-        # search_naive(input, p50)
-        search_ba(input, p50)
-        # search_kmp(input, p50)
-        end_time = time.time()
-        p50_times.append(end_time - start_time)
-    p50_times.sort()
-    print('p50 result median of 50 runs: %f' % p50_times[4])
-    print(p50_times)
-    print()
-
-    p100_times = []
-    for _ in range(10):
-        start_time = time.time()
-        # search_naive(input, p100)
-        search_ba(input, p100)
-        # search_kmp(input, p100)
-        end_time = time.time()
-        p100_times.append(end_time - start_time)
-    p100_times.sort()
-    print('p100 result median of 10 runs: %f' % p100_times[4])
-    print(p100_times)
-    print()
-
-    p500_times = []
-    for _ in range(10):
-        start_time = time.time()
-        # search_naive(input, p500)
-        search_ba(input, p500)
-        # search_kmp(input, p500)
-        end_time = time.time()
-        p500_times.append(end_time - start_time)
-    p500_times.sort()
-    print('p500 result median of 10 runs: %f' % p500_times[4])
-    print(p500_times)
-    print()
-
-    p1000_times = []
-    for _ in range(10):
-        start_time = time.time()
-        # search_naive(input, p1000)
-        search_ba(input, p1000)
-        # search_kmp(input, p1000)
-        end_time = time.time()
-        p1000_times.append(end_time - start_time)
-    p1000_times.sort()
-    print('p1000 result median of 10 runs: %f' % p1000_times[4])
-    print(p1000_times)
-    print()
-
-    p5000_times = []
-    for _ in range(10):
-        start_time = time.time()
-        # search_naive(input, p5000)
-        search_ba(input, p5000)
-        # search_kmp(input, p5000)
-        end_time = time.time()
-        p5000_times.append(end_time - start_time)
-    p5000_times.sort()
-    print('p5000 result median of 10 runs: %f' % p5000_times[4])
-    print(p5000_times)
-    print()
-
-    p10000_times = []
-    for _ in range(5):
-        start_time = time.time()
-        # search_naive(input, p10000)
-        search_ba(input, p10000)
-        # search_kmp(input, p10000)
-        end_time = time.time()
-        p10000_times.append(end_time - start_time)
-    p10000_times.sort()
-    print('p10000 result median of 10 runs: %f' % p10000_times[2])
-    print(p10000_times)
-    print()
-
-    p50000_times = []
-    for _ in range(5):
-        start_time = time.time()
-        # search_naive(input, p50000)
-        search_ba(input, p50000)
-        # search_kmp(input, p50000)
-        end_time = time.time()
-        p50000_times.append(end_time - start_time)
-    p50000_times.sort()
-    print('p50000 result median of 10 runs: %f' % p50000_times[2])
-    print(p50000_times)
-    print()
-
-    p100000_times = []
-    for _ in range(3):
-        start_time = time.time()
-        # search_naive(input, p100000)
-        search_ba(input, p100000)
-        # search_kmp(input, p100000)
-        end_time = time.time()
-        p100000_times.append(end_time - start_time)
-    p100000_times.sort()
-    print('p100000 result median of 3 runs: %f' % p100000_times[1])
-    print(p100000_times)
-    print()
-
-    p500000_times = []
-    for _ in range(3):
-        start_time = time.time()
-        # search_naive(input, p500000)
-        search_ba(input, p500000)
-        # search_kmp(input, p500000)
-        end_time = time.time()
-        p500000_times.append(end_time - start_time)
-    p500000_times.sort()
-    print('p500000 result median of 3 runs: %f' % p500000_times[1])
-    print(p500000_times)
-    print()
-
-    p1000000_times = []
-    for _ in range(3):
-        start_time = time.time()
-        # search_naive(input, p1000000)
-        search_ba(input, p1000000)
-        # search_kmp(input, p1000000)
-        end_time = time.time()
-        p1000000_times.append(end_time - start_time)
-    p1000000_times.sort()
-    print('p1000000 result median of 3 runs: %f' % p1000000_times[1])
-    print(p1000000_times)
-    print()
+            start_time = time.time()
+            search_kmp(input, p)
+            end_time = time.time()
+            kmp_times.append(end_time - start_time)
+        naive_times.sort()
+        ba_times.sort()
+        kmp_times.sort()
+        print('p%i result median of 3 runs of naive, ba and kmp:' % len(p))
+        print('naive: %f' % naive_times[1])
+        print('ba: %f' % ba_times[1])
+        print('kmp: %f\n' % kmp_times[1])
 
 
 def experiments(aInputFile, aPatternFile):
@@ -384,6 +211,45 @@ def experiments(aInputFile, aPatternFile):
     print()
     print(kmp_result)
 
+def verify_search_correctness():
+    input = open('project3/chr1_10000000.txt', 'r', encoding='latin1').read()
+    p10 = open('project3/pattern10.txt', 'r', encoding='latin1').read()
+    p20 = open('project3/pattern20.txt', 'r', encoding='latin1').read()
+    p30 = open('project3/pattern30.txt', 'r', encoding='latin1').read()
+    p40 = open('project3/pattern40.txt', 'r', encoding='latin1').read()
+    p50 = open('project3/pattern50.txt', 'r', encoding='latin1').read()
+    patterns = [p10, p20, p30, p40, p50]
+
+    for p in patterns:
+        naive_result = search_naive(input, p)
+        naive_result.sort()
+        ba_result = search_ba(input, p)
+        ba_result.sort()
+        kmp_result = search_kmp(input, p)
+        kmp_result.sort()
+
+        # comparison reference: python built-in regular expressions
+        # adding 1 as apparently we index from 1 in strings in this course
+        re_result = [m.start() + 1 for m in re.finditer('(?=%s)' % p, input)]
+        re_result.sort()
+
+        match = True
+        if (naive_result != re_result):
+            print('naive differs on pattern %s' % p)
+            match = False
+        if (ba_result != re_result):
+            print('ba differs on pattern %s' % p)
+            match = False
+        if (kmp_result != re_result):
+            print('kmp differs on pattern %s' % p)
+            match = False
+
+        if match:
+            print('all implementations worked on pattern %s' % p)
+        else:
+            print(re_result)
+
+
 def main():
     # print(simple_longest_border('aabcdeaab'))
     # print(border_array('aabcdaaecaabc'))
@@ -392,6 +258,8 @@ def main():
     # print(search_kmp('mississippi', 'ssi'))
     # experiments('project3/chr1_10000000.txt', 'project3/pattern5000.txt')
     verify_naive_Onm()
+    # naive_worstcase_test()
+    # verify_search_correctness()
 
 
 if __name__ == '__main__':
